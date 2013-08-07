d6e120829b7211ac095c372a4dbe5de8d21a95dd
Mario Luan <mariosouzaluan@gmail.com>
2013-08-07 02:35:55 -0300

    testando novas rotas

--- backend/server.js
+++ backend/server.js
@@ -1,39 +1,38 @@
 var express = require('express');
 var app 	= express();
 var mongo 	= require('./mongo');
+var MongoJS = mongo.MongoJS;
 var fs 		= require('fs');
 
 function start(){
 	app.use(express.bodyParser());
 
-	// renderiza
-	app.get('/add-cat', function(req, res){
-		res.sendfile('add-cat.html');
+	// exemplo de POST via form
+	app.post('/add-cat', function( req, res ){
+		var data = req.body.cat;
+
+		var mongoInstance = new MongoJS('merces');
+		mongoInstance.addCategory(data, function( success ){
+			if ( !success ) {
+				console.log( 'cloudnt addCategory' );
+				return false;
+			}
+
+			res.redirect('/list-cat');
+		});
 	});
 
-	// recebe POST data
-	app.post('/add-cat', function(req, res){
-		var postData = req.body.cat;
-
-		// verifica se a categoria já existe no banco
-		mongo.nameExists( postData.name, function( err, exists ){
-			if ( err ) {
-				console.log( err );
-			}
-
-			if ( !exists ) {
-				//salva a categoria no banco
-				mongo.save( postData, function(err, data){
-					if ( err ) {
-						console.log( err );
-					}
-						
-					res.send('data successfully submitted');
-				});
-			} else {
-				res.send('data is already in database');
-			}
+	// exemplo retorno dados
+	app.get('/list-cat', function(req, res){
+		var mongoInstance = new MongoJS('merces');
+		mongoInstance.findAll('cat', function( data ){
+			res.send( data );
 		});
+	});
+
+	// exemplo renderizacao HTML
+	app.get('/add-cat', function(req, res){
+		res.sendfile('add-cat.html');
 	});
 
 	// starta o server
