cafaae964a72fd2b50992ac51ba6d65f1a0217cb
Mario Luan <mariosouzaluan@gmail.com>
2013-08-07 02:37:18 -0300

    config min css via compass

--- config.rb
+++ config.rb
@@ -5,7 +5,7 @@ http_path = "/vila-das-merces/"
 css_dir = "css"
 sass_dir = "sass"
 images_dir = "img"
-javascripts_dir = "js"
+javascripts_dir = "javascripts"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
