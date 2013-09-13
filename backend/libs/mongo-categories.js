6f04bfa62ea730c4d66f287506d89d455ebd5ec7
Mario Luan <mariosouzaluan@gmail.com>
2013-09-12 21:39:19 -0300

    criando lib para manipular categorias de places

---
+++ backend/libs/mongo-categories.js
@@ -0,0 +1,55 @@
+var MongoJS = require('../libs/mongo').MongoJS;
+
+function MongoJSCategory(){
+	/*
+	**  Esta classe serve para fazer CRUD com categorias
+	*/
+
+	this._super 		= new MongoJS( 'merces' );
+	this.collectionName = 'category';
+}
+
+MongoJSCategory.prototype.add = function( jsonquery, callback ){
+	/*
+	** Adiciona uma categoria na collection
+	** @param {Object} 		: query
+	** @callback {Function} : a categoria foi adicionada?
+	*/
+
+	var self 	= this;
+	var query 	= jsonquery;
+
+	self._super.insert( query, self.collectionName, function( status ){
+		callback( status );
+	});
+};
+
+MongoJSCategory.prototype.remove = function( jsonquery, callback ){
+	/*
+	** Remove uma categoria da collection
+	** @param {Object} 		: query
+	** @callback {Function} : a categoria foi removida?
+	*/
+
+	var self 	= this;
+	var query 	= jsonquery;
+
+	self._super.remove( query, self.collectionName, function( status ){
+		callback( status );
+	});
+};
+
+MongoJSCategory.prototype.list = function( callback ){
+	/*
+	** Lista as categorias da collection
+	** @callback {Function} : json
+	*/
+
+	var self 	= this;
+
+	self._super.findAll( self.collectionName, function( status ){
+		callback( status );
+	});
+};
+
+exports.MongoJSCategory = MongoJSCategory;
