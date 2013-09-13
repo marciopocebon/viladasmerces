f37708bdc3136ea171e5e791f2f1a3764f7ccdc7
Mario Luan <mariosouzaluan@gmail.com>
2013-09-12 21:39:48 -0300

    criando rota para admin de categorias de places

---
+++ backend/routes/categories.js
@@ -0,0 +1,10 @@
+var MongoJSCategory = require('../libs/mongo-categories').MongoJSCategory;
+
+exports.list = function( req, res ){
+	console.log( req.params );
+	res.send( 's' );
+	// var obj = new MongoJSCategory();
+	// obj.list(function( data ){
+	// 	res.send( data );
+	// });
+};
