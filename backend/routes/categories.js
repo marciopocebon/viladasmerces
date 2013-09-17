3f8dc9ec6bcf74f35520723f291b26121739fb84
Mario Luan <mariosouzaluan@gmail.com>
2013-09-17 01:39:45 -0300

    alterando nome de metodos para melhorar leitura

--- backend/routes/categories.js
+++ backend/routes/categories.js
@@ -1,10 +1,23 @@
 var MongoJSCategory = require('../libs/src/mongo-categories').MongoJSCategory;
+var categories 		= new MongoJSCategory();
+var url 			= require('url');
 
-exports.list = function( req, res ){
-	console.log( req.params );
-	res.send( 's' );
-	// var obj = new MongoJSCategory();
-	// obj.list(function( data ){
-	// 	res.send( data );
-	// });
+exports.findAll = function( req, res ) {
+	/*
+	**  lista todas as categories
+	*/
+	categories.findAll(function( data ){
+		res.send( data );
+	});
+};
+
+exports.add = function( req, res ) {
+	/*
+	**  cadastra uma nova categoria
+	*/
+	var query = url.parse( req.url, true ).query;
+	
+	categories.add(query, function( status ){
+		res.send( status );
+	});
 };
