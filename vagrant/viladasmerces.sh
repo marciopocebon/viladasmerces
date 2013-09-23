2f3fb812ddb6f0e8807d8608997278cfd5c688c1
Mario Luan <mariosouzaluan@gmail.com>
2013-09-22 23:43:16 -0300

    e necessario que o arquivo seja executavel para ser utilizado como um service

--- vagrant/viladasmerces.sh
+++ vagrant/viladasmerces.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 #
-# viladasmerces: 21 09 13
-# description: Configuration to run the app
-#
+# processname: viladasmerces
+# description: Configuration script to run the app
+# usage : sudo service viladasmerces ( start | stop | restart )
 
 start() {
         ### start the app ###
-        logger "echo -n Starting viladasmerces server: "
+        echo $"Starting viladasmerces application: "
         cd /srv/www/viladasmerces && npm start
         ### Creates the lock file ###
         touch /var/lock/viladasmerces
-        success $"viladasmerces server startup"
         echo
 }
 
 stop() {
         ### stop the app ###
-        logger "echo -n Stopping viladasmerces server: "
+        echo $"Stopping viladasmerces server: "
         cd /srv/www/viladasmerces && npm stop
 
         ### Deletes the lock file ###
