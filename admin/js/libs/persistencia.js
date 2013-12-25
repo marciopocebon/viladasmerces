cd4ef2d979083583821c0f24614bd6b841633edc
Mario Luan <mariosouzaluan@gmail.com>
2013-12-25 21:00:06 -0200

    criando constante da rota para nao repetir codigo entre metodos

--- admin/js/libs/persistencia.js
+++ admin/js/libs/persistencia.js
@@ -2,22 +2,23 @@
  * responsavel por interagir front com o  banco de dados
  */
 function Persistencia( colecao ){
-	this.colecao = colecao;
+	this.colecao 	= colecao;
+	this.rota 		= 'http://app.viladasmerces/rest/';
 }
 
 Persistencia.prototype.recuperarItens = function( callback ){
 	var self = this;
 	var metodo = 'GET';
-	var rota = 'http://app.viladasmerces/rest/'+self.colecao;
+	var rota = self.rota+self.colecao;
 	var dados = {};
 
 	Network.ajax( metodo, rota, dados, callback );
 };
 
-Persistencia.prototype.recuperarItem = function( id, callback ){
+Persistencia.prototype.recuperarItem = function( nome, callback ){
 	var self = this;
 	var metodo = 'GET';
-	var rota = 'http://app.viladasmerces/rest/'+self.colecao+'/'+id;
+	var rota = self.rota+self.colecao+'/'+nome;
 	var dados = {};
 
 	Network.ajax( metodo, rota, dados, callback );
@@ -26,25 +27,25 @@ Persistencia.prototype.recuperarItem = function( id, callback ){
 Persistencia.prototype.adicionarItem = function( dados, callback ){
 	var self = this;
 	var metodo = 'POST';
-	var rota = 'http://app.viladasmerces/rest/'+self.colecao;
+	var rota = self.rota+self.colecao;
 	var dados = dados;
 
 	Network.ajax( metodo, rota, dados, callback );
 };
 
-Persistencia.prototype.atualizarItem = function( id, dados, callback ){
+Persistencia.prototype.atualizarItem = function( nome, dados, callback ){
 	var self = this;
 	var metodo = 'PUT';
-	var rota = 'http://app.viladasmerces/rest/'+self.colecao+'/'+id;
+	var rota = self.rota+self.colecao+'/'+nome;
 	var dados = dados;
 
 	Network.ajax( metodo, rota, dados, callback );
 };
 
-Persistencia.prototype.removerItem = function( id, callback ){
+Persistencia.prototype.removerItem = function( nome, callback ){
 	var self = this;
 	var metodo = 'DELETE';
-	var rota = 'http://app.viladasmerces/rest/'+self.colecao+'/'+id;
+	var rota = self.rota+self.colecao+'/'+nome;
 	var dados = {};
 
 	Network.ajax( metodo, rota, dados, callback );
