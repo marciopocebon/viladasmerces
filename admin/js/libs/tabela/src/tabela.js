4d9c3371aef00b7b8eed374bfba851068f12cafa
Mario Luan <mariosouzaluan@gmail.com>
2013-12-25 20:54:43 -0200

    criando lista de itens adicionados a tabela para nao precisar percorrer HTML a cada mudanca

--- admin/js/libs/tabela/src/tabela.js
+++ admin/js/libs/tabela/src/tabela.js
@@ -2,35 +2,34 @@ function Tabela( container ){
 	this.$container 	= $( container );
 	this.$header 		= this.$container.find('thead');
 	this.$body 			= this.$container.find('tbody');
+	this.itens 			= {};
 }
 
-Tabela.prototype.addItem = function( jsonItem ){
-	var item = new Item( jsonItem );
+Tabela.prototype.addItem = function( itemJson ){
+	var item = null;
+	
+	if ( itemJson instanceof Item ) item = itemJson;
+	else item = new Item( itemJson );
+
 	this.$body.prepend( item.$container );
+	this.itens[item.ID] = item;
 };
 
-Tabela.prototype.addItens = function( jsonItens ){
+Tabela.prototype.addItens = function( itensJson ){
 	var self = this;
-	var length = jsonItens.length;
+	var quantidade = itensJson.length;
 	var fragment = $(document.createDocumentFragment());
 
-	for ( var i = 0 ; i < length ; i++ ) {
-		var jsonItem = jsonItens[i];
-		var item = new Item( jsonItem );
-
-		item.escutarBtnRemover(function( itemClicado ){
-			self.removerItem( itemClicado, function( idItemRemovido ){
-				console.log( idItemRemovido );
-			});
-		});
-		
+	for ( var i = 0 ; i < quantidade ; i++ ) {
+		var item = new Item( itensJson[i] );
 		fragment.append( item.$container );
+		self.itens[item.ID] = item;
 	}
 
-	this.$container.append( fragment );
+	self.$body.append( fragment );
 };
 
-Tabela.prototype.removerItem = function( item, callback ){
+Tabela.prototype.rmItem = function( item ){
 	item.$container.remove();
-	callback( item.ID );
+	delete this.itens[item.ID];
 };
