d1eb88514b1a354bb79b380b3b04f3c61b93b63b
Mario Luan <mariosouzaluan@gmail.com>
2014-01-04 21:46:44 -0200

    adicionando lib generica helper

--- admin/js/categorias.js
+++ admin/js/categorias.js
@@ -1,4 +1,4 @@
-var deps = ['libs/jquery', 'libs/network', 'libs/persistencia', 'libs/formulario', 'libs/tabela/min/index', 'libs/gerenciador-crud'];
+var deps = ['libs/jquery', 'libs/helper', 'libs/network', 'libs/persistencia', 'libs/formulario', 'libs/tabela/min/index', 'libs/gerenciador-crud'];
 
 requirejs(deps, function(){
 	var config = {
